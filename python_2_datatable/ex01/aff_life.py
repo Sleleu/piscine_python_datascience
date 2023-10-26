f02acadc80923facd128d179f17dbca57b548c17
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2023-10-26 21:08:06 +0200

    continue ex01

---
+++ python_2_datatable/ex01/aff_life.py
@@ -0,0 +1,25 @@
+from load_csv import load
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def aff_life(df: pd.DataFrame, country: str):
+    france_data = df[df["country"] == country].squeeze()
+    france_years = france_data.drop("country")
+    plt.plot(france_years.index, france_years.values)
+    plt.title(f"{country} Life expectancy Projections")
+    plt.xlabel("Year")
+    plt.ylabel("Life expectancy")
+    plt.xticks(range(0, 360, 40))
+    plt.show()
+
+def main():
+    df = load("life_expectancy_years.csv")
+    try:
+        aff_life(df, "France")
+    except (KeyError, TypeError) as error:
+        print(f"{__name__}: {type(error).__name__}: {error}")
+
+
+if __name__ == "__main__":
+    main()
